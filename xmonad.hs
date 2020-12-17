18f50ca217cc0e3d03649ba96eb3c42ed29673ac
haoxiangliew <haoxiangliew@gmail.com>
2020-12-16 20:54:29 -0800

    update configurations

--- xmonad.hs
+++ xmonad.hs
@@ -8,23 +8,34 @@ import XMonad.Actions.SpawnOn
 import Graphics.X11.ExtraTypes.XF86
 import System.IO
 import XMonad.Layout.Grid
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.DynamicBars
+import XMonad.Util.Spotify
 
 main = do
     --setRandomWallpaper ["$HOME/Pictures/nixos.png"]
 
     xmproc <- spawnPipe "xmobar"
 
-    xmonad $ docks defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
+    xmonad $ docks $ ewmh defaultConfig
+        { manageHook = manageDocks <+> (isFullscreen --> doFullFloat) <+> (className =? "mpv" --> doFloat) <+> manageHook defaultConfig
         , startupHook = do
-                spawnHere "xloadimage -onroot -fullscreen ~/Pictures/nixos.png"
+                spawn "picom -b"
+                spawnHere "feh --bg-scale ~/Pictures/nixos.png"
                 -- spawnOn "workspace2" "pulseeffects"
+                spawn "pulseeffects --gapplication-service"
+                spawn "numlockx"
+                spawn "emacs --daemon"
+                spawn "ibus-daemon"
+                spawn "xautolock -detectsleep -time 15 -locker \"physlock -m -s -d\" -nowlocker \"physlock -m -s -d\" -killtime 20 -killer \"systemctl suspend\""
         , layoutHook = avoidStruts  $ layoutHook defaultConfig ||| Grid
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "#98c379" "" . shorten 100
                         , ppCurrent = xmobarColor "#61afef" "" . wrap "[" "]"
                         }
+        , handleEventHook    = fullscreenEventHook
         , borderWidth        = 1
         , modMask            = mod4Mask
         , terminal           = "alacritty"
@@ -32,7 +43,22 @@ main = do
         , focusedBorderColor = "#4B5363" }
         `additionalKeys`
         [
-          ((mod4Mask, xK_p                   ), spawn "dmenu_run -fn 'monospace:regular:pixelsize=12' -nb '#282c34' -sf '#282c34' -sb '#98c379' -nf '#abb2bf'" ) ,
+          ((mod4Mask, xK_p                   ), spawn "dmenu_run -fn 'monospace:regular:pixelsize=12' -nb '#282c34' -sf '#282c34' -sb '#98c379' -nf '#abb2bf'" ),
+          ((mod4Mask, xK_c                   ), spawn "~/caffeine.sh"),
+          ((mod4Mask, xK_Return              ), spawn "emacsclient -c"),
+          ((mod4Mask, xK_b                   ), spawn "chromium --enable-features=WebUIDarkMode --force-dark-mode"),
+          ((mod4Mask, xK_n                   ), spawn "chromium -incognito --enable-features=WebUIDarkMode --force-dark-mode"),
+          ((mod4Mask, xK_l                   ), spawn "xautolock -locknow"),
+          ((mod4Mask, xK_s                   ), spawn "maim -s -u | xclip -selection clipboard -t image/png -i"),
+          ((mod4Mask .|. shiftMask, xK_s     ), spawn "maim -s"),
+          ((mod4Mask, xK_f                   ), spawn "alacritty -e ranger"),
+          ((mod4Mask, xK_m                   ), spawn "emacsclient -c -e '(=mu4e)'"),
+          ((mod4Mask, xK_a                   ), spawn "emacsclient -c -e '(org-agenda)'"),
+          ((mod4Mask, xK_Up                  ), audioPlayPause),
+          ((mod4Mask, xK_Down                ), audioStop),
+          ((mod4Mask, xK_Left                ), audioPrev),
+          ((mod4Mask, xK_Right               ), audioNext),
+          ((0       , xF86XK_Calculator       ), spawn "alacritty -e python3"),
           ((0       , xF86XK_AudioMute        ), spawn "amixer set Master toggle"),
           ((0       , xF86XK_AudioLowerVolume ), spawn "amixer -q set Master 2%-"),
           ((0       , xF86XK_AudioRaiseVolume ), spawn "amixer -q sset Master 2%+"),
